58a97f879ff58fb143cc31fe1807315026b85a76
ravlyseven <ravlyseven@gmail.com>
2021-12-05 14:54:21 +0700

    selesai (belum revisi)

--- resources/views/compare/result.blade.php
+++ resources/views/compare/result.blade.php
@@ -5,15 +5,20 @@
     <div class="container pb-5" style="margin-top:100px;">
         <div class="row justify-content-center">  
 
+        <?php
+            $peringkat = 0;
+        ?>
         @foreach($data as $objek)
             <?php
+            $peringkat++;
             $item = App\Objek::where('id', $objek['id_objek'])->first();
             ?>
             <div class="col-lg-4 col-sm-6 col-12">
                 <div class="card">
                     <!-- Card image -->
                     <img class="card-img my-2" src="{{ asset('storage/' . $item->gambar) }}" alt="Image placeholder" style="height:200px; object-fit: contain;">
-                    <span class="product-tags">{{ substr($objek['bobot'],0,4) }}</span>
+                    <span class="product-tags">Rank : {{ $peringkat }}</span>
+                    <span class="product-tags mt-4">{{ substr($objek['bobot'],0,4) }}</span>
                     <!-- Card body -->
                     <div class="card-body">
                         <h4 class="card-title mb-3">{{ $item->merk . ' ' . $item->type }}</h4>
